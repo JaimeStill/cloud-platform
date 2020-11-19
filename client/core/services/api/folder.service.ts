2572edcf391645d32816408c7c836a10727659db
aaronjcolclough <aaron.j.colclough@outlook.com>
2020-11-19 10:13:19 -0500

    added note and folder services

---
+++ client/core/services/api/folder.service.ts
@@ -0,0 +1,94 @@
+import {
+  Injectable,
+  Optional
+} from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { SnackerService } from '../../services';
+import { ServerConfig } from '../../config';
+import { Folder } from 'client/core/models';
+
+@Injectable()
+export class FolderService {
+  private folders = new BehaviorSubject<Folder[]>(null);
+  private folder = new BehaviorSubject<Folder>(null);
+
+  folders$ = this.folders.asObservable();
+  folder$ = this.folder.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private snacker: SnackerService,
+    @Optional() private config: ServerConfig
+  ) { }
+
+  getFolders = (userId: number) => this.http.get<Folder[]>(`${this.config.api}folder/getFolders/${userId}`)
+    .subscribe(
+      data => this.folders.next(data),
+      err => this.snacker.sendErrorMessage(err.error)
+    )
+
+  searchFolders = (userId: number, search: string) =>
+    this.http.get<Folder[]>(`${this.config.api}folder/searchFolders/${userId}/${search}`)
+      .subscribe(
+        data => this.folders.next(data),
+        err => this.snacker.sendErrorMessage(err.error)
+      )
+
+  getFolder = (id: number): Promise<Folder> => new Promise((resolve) => {
+    this.http.get<Folder>(`${this.config.api}/folder/getFolder/${id}`)
+      .subscribe(
+        data => {
+          this.folder.next(data);
+          resolve(data);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(null);
+        }
+      );
+  })
+
+  addFolder = (folder: Folder): Promise<Folder> => new Promise((resolve) => {
+    this.http.post<Folder>(`${this.config.api}/folder/addFolder`, folder)
+      .subscribe(
+        data => {
+          this.snacker.sendSuccessMessage(`${folder.name} successfully created`);
+          resolve(data);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(null);
+        }
+      );
+  })
+
+  updateFolder = (folder: Folder): Promise<boolean> => new Promise((resolve) => {
+    this.http.post(`${this.config.api}/folder/updateFolder`, folder)
+      .subscribe(
+        () => {
+          this.snacker.sendSuccessMessage(`${folder.name} successfully updated`);
+          resolve(true);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(false);
+        }
+      );
+  })
+
+  removeFolder = (folder: Folder): Promise<boolean> => new Promise((resolve) => {
+    this.http.post(`${this.config.api}folder/removeFolder`, folder)
+      .subscribe(
+        () => {
+          this.snacker.sendSuccessMessage(`${folder.name} successfully removed`);
+          resolve(true);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(false);
+        }
+      );
+  })
+}
---
+++ client/core/services/api/note.service.ts
@@ -0,0 +1,94 @@
+import {
+  Injectable,
+  Optional
+} from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Note } from '../../models';
+import { SnackerService } from '../../services';
+import { ServerConfig } from '../../config';
+
+@Injectable()
+export class NoteService {
+  private notes = new BehaviorSubject<Note[]>(null);
+  private note = new BehaviorSubject<Note>(null);
+
+  notes$ = this.notes.asObservable();
+  note$ = this.note.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private snacker: SnackerService,
+    @Optional() private config: ServerConfig
+  ) { }
+
+  getNotes = (folderId: number) => this.http.get<Note[]>(`${this.config.api}/note/getNotes/${folderId}`)
+    .subscribe(
+      data => this.notes.next(data),
+      err => this.snacker.sendErrorMessage(err.error)
+    )
+
+  searchNotes = (folderId: number, search: string) =>
+    this.http.get<Note[]>(`${this.config.api}note/searchNotes/${folderId}/${search}`)
+      .subscribe(
+        data => this.notes.next(data),
+        err => this.snacker.sendErrorMessage(err.error)
+      )
+
+  getNote = (id: number): Promise<Note> => new Promise((resolve) => {
+    this.http.get<Note>(`${this.config.api}/note/getNote/${id}`)
+      .subscribe(
+        data => {
+          this.note.next(data);
+          resolve(data);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(null);
+        }
+      );
+  })
+
+  addNote = (note: Note): Promise<Note> => new Promise((resolve) => {
+    this.http.post<Note>(`${this.config.api}note/addNote`, note)
+      .subscribe(
+        data => {
+          this.snacker.sendSuccessMessage(`${note.title} successfully created`);
+          resolve(data);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(null);
+        }
+      );
+  })
+
+  updateNote = (note: Note): Promise<boolean> => new Promise((resolve) => {
+    this.http.post(`${this.config.api}/note/updateNote`, note)
+      .subscribe(
+        () => {
+          this.snacker.sendSuccessMessage(`${note.title} successfully updated`);
+          resolve(true);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(false);
+        }
+      );
+  })
+
+  removeNote = (note: Note): Promise<boolean> => new Promise((resolve) => {
+    this.http.post(`${this.config.api}note/removeNote`, note)
+      .subscribe(
+        () => {
+          this.snacker.sendSuccessMessage(`${note.title} successfully removed`);
+          resolve(true);
+        },
+        err => {
+          this.snacker.sendErrorMessage(err.error);
+          resolve(false);
+        }
+      );
+  })
+}
